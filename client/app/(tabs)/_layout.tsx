dda759136ccaa4a688d2deedfc7d9d256bc3884a
dongzoolee <me@leed.at>
2025-09-25 15:32:17 +0900

    Hide tabBar

--- client/app/(tabs)/_layout.tsx
+++ client/app/(tabs)/_layout.tsx
@@ -13,7 +13,7 @@ export default function TabLayout() {
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
         headerShown: false,
-        tabBarStyle: { display: 'none' },
+        tabBarStyle: { display: "none" },
       }}
     >
       <Tabs.Screen
