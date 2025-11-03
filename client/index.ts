ef47a80543a525824dbd40194bfd38c800b51744
dongzoolee <me@leed.at>
2025-11-03 12:03:00 +0900

    Fix a bug where react-native-skia is not working on ios

--- client/index.ts
+++ client/index.ts
@@ -1,9 +1,1 @@
-import "@expo/metro-runtime";
-import { App } from "expo-router/build/qualified-entry";
-import { renderRootComponent } from "expo-router/build/renderRootComponent";
-
-import { LoadSkiaWeb } from "@shopify/react-native-skia/lib/module/web";
-
-LoadSkiaWeb().then(async () => {
-  renderRootComponent(App);
-});
+import "expo-router/entry";
