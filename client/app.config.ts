66083a37e41d8732e110bc775da0dccbe6d84237
dongzoolee <me@leed.at>
2025-11-17 18:07:18 +0900

    Fix `✔ iOS app only uses standard/exempt encryption? Learn more … yes`

--- client/app.config.ts
+++ client/app.config.ts
@@ -32,6 +32,7 @@ const config: ExpoConfig = {
       NSAppTransportSecurity: {
         NSAllowsArbitraryLoads: true,
       },
+      ITSAppUsesNonExemptEncryption: false,
     },
   },
   android: {
