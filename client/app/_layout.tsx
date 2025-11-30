9de79480c3f3e52e29c196729005508d124bdef1
dongzoolee <me@leed.at>
2025-11-30 21:24:45 +0900

    Show title on web

--- client/app/_layout.tsx
+++ client/app/_layout.tsx
@@ -1,11 +1,17 @@
 import { Stack } from "expo-router";
+import Head from "expo-router/head";
+import Constants from "expo-constants";
 
 import "../global.css";
 import { AppProvider } from "@/providers/AppProvider";
 
 export default function RootLayout() {
+  const title = Constants.expoConfig?.web?.name;
   return (
     <AppProvider>
+      <Head>
+        <title>{title}</title>
+      </Head>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
